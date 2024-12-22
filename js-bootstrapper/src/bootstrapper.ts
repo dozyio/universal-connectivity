4b706b79102302998692c5b4f8daf9570cfee0d8
dozyio <github@dozy.io>
2024-12-22 23:53:14 +0000

    remove some services

--- js-bootstrapper/src/bootstrapper.ts
+++ js-bootstrapper/src/bootstrapper.ts
@@ -238,29 +238,29 @@ async function msgIdFnStrictNoSign(msg: Message): Promise<Uint8Array> {
         identifyPush: identifyPush(),
         ping: ping(),
         pubsub: gossipsub(gossipsubConfig),
-        relay: circuitRelayServer(),
+        // relay: circuitRelayServer(),
         keychain: keychain(),
         autoTLS: autoTLS({
           acmeDirectory: 'https://acme-staging-v02.api.letsencrypt.org/directory',
           autoConfirmAddress: true
         }),
-        upnpNAT: uPnPNAT({
-          autoConfirmAddress: true
-        }),
+        // upnpNAT: uPnPNAT({
+        //   autoConfirmAddress: true
+        // }),
         autoNAT: autoNAT(),
-        aminoDHT: kadDHT({
-          protocol: '/ipfs/kad/1.0.0',
-          peerInfoMapper: removePrivateAddressesMapper
-        }),
-        bootstrap: bootstrap({
-          list: [
-            '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
-            '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
-            '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
-            '/dnsaddr/va1.bootstrap.libp2p.io/p2p/12D3KooWKnDdG3iXw9eTFijk3EWSunZcFi54Zka4wmtqtt6rPxc8',
-            '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ'
-          ]
-        })
+        // aminoDHT: kadDHT({
+        //   protocol: '/ipfs/kad/1.0.0',
+        //   peerInfoMapper: removePrivateAddressesMapper
+        // }),
+        // bootstrap: bootstrap({
+        //   list: [
+        //     '/dnsaddr/bootstrap.libp2p.io/p2p/QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN',
+        //     '/dnsaddr/bootstrap.libp2p.io/p2p/QmbLHAnMoJPWSCR5Zhtx6BHJX9KiKNN6tpvbUcqanj75Nb',
+        //     '/dnsaddr/bootstrap.libp2p.io/p2p/QmcZf59bWwK5XFi76CZX8cbJ4BhTzzA3gU1ZjYZcYW3dwt',
+        //     '/dnsaddr/va1.bootstrap.libp2p.io/p2p/12D3KooWKnDdG3iXw9eTFijk3EWSunZcFi54Zka4wmtqtt6rPxc8',
+        //     '/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ'
+        //   ]
+        // })
       },
     }
 
